e582450414f9988cd459439d4cc438bc6575ac79
Joona <joona.tyni@edu.salpaus.fi>
2024-10-28 10:38:55 +0200

    #

---
+++ styling/script.js
@@ -0,0 +1,9 @@
+var body = document.querySelector("body"),
+
+function changetheme() {
+    if (body.className === "") {
+        body.classList.add("dark")
+    } else {
+        body.classList.remove("dark")
+    }
+}
